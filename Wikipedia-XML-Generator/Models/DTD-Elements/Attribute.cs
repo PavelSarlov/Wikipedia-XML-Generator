aa039e4e2ff821e7025dcb136b2cc969954afc42
Мирослав Дионисиев <miro9122@gmail.com>
2021-12-05 23:17:03 +0200

    Created Attribute class

---
+++ Wikipedia-XML-Generator/Models/DTD-Elements/Attribute.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wikipedia_XML_Generator.Models.Enums;
+
+namespace Wikipedia_XML_Generator.Models.DTD_Elements
+{
+    public class Attribute
+    {
+        private String name;
+        private AttributeTypes type;
+        private AttributeValuesType valuesType;
+        private List<String> enumerations;
+        private String value;
+
+        public Attribute(String name, AttributeTypes type, AttributeValuesType valuesType = AttributeValuesType.REQUIRED, List<String> enumerations = null, String value = null)
+        {
+            this.name = name;
+            this.type = type;
+            this.enumerations = enumerations;
+            this.valuesType = valuesType;
+            if(this.validateValue(value) == true)
+            {
+                this.value = value;
+            }
+        }
+
+        private bool validateValue(String value)
+        {
+            if(this.type == AttributeTypes.ENUMERATION)
+            {
+                return this.enumerations.Contains(value);
+            }
+            return true;
+        }
+
+        public String Name { get; private set; }
+        public AttributeTypes Type { get; private set;}
+        public AttributeValuesType ValueTypes { get; private set; }
+        public String Values 
+        {
+            get { return this.value; }
+            set {
+                if (this.validateValue(value) == true)
+                {
+                    this.value = value;
+                }
+            } 
+        }
+    }
+}
---
+++ Wikipedia-XML-Generator/Models/Enums/AttributeTypes.cs
@@ -0,0 +1,16 @@
+﻿namespace Wikipedia_XML_Generator.Models.Enums
+{
+    public enum AttributeTypes
+    {
+        CDATA = 1,
+        ID = 2,
+        IDREF = 3,
+        IDREFS = 4,
+        NMTOKEN = 5,
+        NMTOKENS = 6,
+        ENTITY = 7,
+        ENTITIES = 8,
+        NOTATION = 9,
+        ENUMERATION = 10
+    }
+}
