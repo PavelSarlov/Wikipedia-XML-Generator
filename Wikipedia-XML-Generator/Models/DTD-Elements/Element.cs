d945280d2036cd901df78d349e7b3a6564df205b
Мирослав Дионисиев <miro9122@gmail.com>
2021-12-06 11:20:48 +0200

    Updated Element class methods

--- Wikipedia-XML-Generator/Models/DTD-Elements/Element.cs
+++ Wikipedia-XML-Generator/Models/DTD-Elements/Element.cs
@@ -9,14 +9,14 @@ namespace Wikipedia_XML_Generator.Models.DTD_Elements
     {
         private String name;
         private String content;
-        Dictionary<String, int> childrenOccurrences;
-        Dictionary<String, char> childrenQuantifies;
+        Dictionary<String, Tuple<int, char>> childrenOccurrences;
+//        Dictionary<String, char> childrenQuantifies;
+        Dictionary<List<String>, Tuple<int, char>> childrenInGroupOccurrences;
 
         public Element(String name, Dictionary<String, char> childrenQuantifies, String content = null)
         {
             this.name = name;
-            this.childrenQuantifies = childrenQuantifies;
-            this.popuateOccurrencesDictionary();
+            this.popuateOccurrencesDictionary(childrenQuantifies);
             if(this.validateContent(content) == true)
             {
                 this.content = content;
@@ -29,49 +29,76 @@ namespace Wikipedia_XML_Generator.Models.DTD_Elements
 
         private bool validateContent(String content)
         {
-            if(this.childrenQuantifies.ContainsKey("#PCDATA"))
+            if(this.childrenOccurrences.ContainsKey("#PCDATA"))
             {
                 return true;
             }
             return false;
         }
 
-        private void popuateOccurrencesDictionary()
+        private void popuateOccurrencesDictionary(Dictionary<String, char> childrenQuantifies)
         {
-            foreach(var item in this.childrenQuantifies)
+            foreach(var item in childrenQuantifies)
             {
-                this.childrenOccurrences[item.Key] = 0;
+                if(item.Key[0]!='(')
+                {
+                    this.childrenOccurrences[item.Key] = new Tuple<int, char>(0, item.Value);
+                }
+                else
+                {
+                    List<String> elements = item.Key.Remove(0,1).Remove(item.Key.Length-1, 1).Split('|').ToList<String>();
+                    this.childrenInGroupOccurrences[elements] = new Tuple<int, char>(0, item.Value);
+                }
             }
-        }
-
-        public bool isAChild(String elementName)
-        {
-            return this.childrenQuantifies.ContainsKey(elementName);
         }
 
         public bool addChild(String elementName)
         {
-            if (isAChild(elementName))
+            if (this.childrenOccurrences.ContainsKey(elementName))
             {
-                this.childrenOccurrences[elementName]++;
+                var valTuple = this.childrenOccurrences[elementName];
+                this.childrenOccurrences[elementName] = new Tuple<int, char>(valTuple.Item1 + 1, valTuple.Item2);
                 return true;
+            }
+            foreach (var item in this.childrenInGroupOccurrences)
+            {
+                if (item.Key.Contains(elementName))
+                {
+                    this.childrenInGroupOccurrences[item.Key] = new Tuple<int, char>(item.Value.Item1 + 1, item.Value.Item2);
+                    return true;
+                }
             }
             return false;
         }
 
         public bool isChildrenCountValid()
         {
-            foreach(var item in this.childrenQuantifies)
+            foreach(var item in this.childrenOccurrences)
             {
-                if(item.Value == '+' && this.childrenOccurrences[item.Key]<1)
+                if (item.Value.Item2 == '+' && item.Value.Item1 < 1)
                 {
                     return false;
                 }
-                else if (item.Value == '?' && this.childrenOccurrences[item.Key] > 1)
+                else if (item.Value.Item2 == '?' && item.Value.Item1 > 1)
                 {
                     return false;
                 }
-                if (item.Value == ' ' && this.childrenOccurrences[item.Key] != 1)
+                if (item.Value.Item2 == ' ' && item.Value.Item1 != 1)
+                {
+                    return false;
+                }
+            }
+            foreach (var item in this.childrenInGroupOccurrences)
+            {
+                if (item.Value.Item2 == '+' && item.Value.Item1 < 1)
+                {
+                    return false;
+                }
+                else if (item.Value.Item2 == '?' && item.Value.Item1 > 1)
+                {
+                    return false;
+                }
+                if (item.Value.Item2 == ' ' && item.Value.Item1 != 1)
                 {
                     return false;
                 }
